aa2bb73414505eda420810014391fe498e7d912f
ComputerScienceSean <seangunnar96@gmail.com>
2019-06-01 18:57:04 +0200

    added pay bill activity

---
+++ app/src/main/java/com/example/bankapp/activities/PayBills.java
@@ -0,0 +1,15 @@
+package com.example.bankapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bankapp.R;
+
+public class PayBills extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_bills);
+    }
+}
